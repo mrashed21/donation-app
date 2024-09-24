cc45cae4a245309e57c0135d1b4866ebdf0a26af
mrashed21 <muhammadrashed760@gmail.com>
2024-09-24 18:38:29 +0600

    updated

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -27,6 +27,7 @@ const donateNoakhali = donateSumbit("donateNoakhali");
 donateNoakhali.addEventListener("click", () => {
   let amountNoakhali = getdonateAmount("amountNoakhali");
   let initialBalanceNoakhali = donatInitial("initialBalanceNoakhali");
+
   if (
     amountNoakhali === "" ||
     isNaN(amountNoakhali) ||
@@ -150,6 +151,75 @@ donateFeni.addEventListener("click", () => {
     );
     div.innerHTML = `<h2 class="font-medium text-xl px-4 mt-4 primary-txt">
               ${amountFeni} Donate for Flood Relief in Feni,Bangladesh
+            </h2>
+            <p
+              class="text-base font-light secondarytxt px-4 mb-4"
+            >
+              Time: ${time} Date: ${date}
+            </p> `;
+    history.appendChild(div);
+  }
+});
+
+/*****Donate for Quota*****/
+const donateQuota = donateSumbit("donateQuota");
+donateQuota.addEventListener("click", () => {
+  const amountQuota = getdonateAmount("amountQuota");
+  let initialBalanceQuota = donatInitial("initialBalanceQuota");
+  if (
+    amountQuota === "" ||
+    isNaN(amountQuota) ||
+    amountQuota <= 0 ||
+    amountQuota > initialBalance
+  ) {
+    const modal = document.getElementById("donateFailed");
+    const closePopUp = document.getElementById("close-popup");
+    modal.close();
+    modal.classList.add("hidden");
+    modal.classList.remove("hidden");
+    modal.showModal();
+    closePopUp.addEventListener("click", function () {
+      document.getElementById("donateFailed").classList.add("hidden");
+    });
+  } else {
+    /* Update Donate Amount */
+    let result = parseFloat(initialBalanceQuota) + amountQuota;
+    document.getElementById("initialBalanceQuota").innerText = result;
+
+    /*update Current Balance*/
+    initialBalance -= amountQuota;
+    document.getElementById("initialBalance").innerText = initialBalance;
+
+    /*Donation Success Modal*/
+    const modal = document.getElementById("donateSuccess");
+    const closePopUp = document.getElementById("close-popup");
+    modal.close();
+    modal.classList.add("hidden");
+    modal.classList.remove("hidden");
+    modal.showModal();
+    closePopUp.addEventListener("click", function (e) {
+      document.getElementById("donateFailed").classList.add("hidden");
+    });
+
+    /*Donation History*/
+    const timeDate = new Date();
+    const time = timeDate.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    const date = timeDate.toLocaleDateString("en-US");
+    const history = document.getElementById("historyData");
+    const div = document.createElement("div");
+    div.classList.add(
+      "bg-white",
+      "shadow-md",
+      "rounded-lg",
+      "border-2",
+      "border-gray-300"
+    );
+    div.innerHTML = `<h2 class="font-medium text-xl px-4 mt-4 primary-txt">
+              ${amountQuota} Donate for Flood Relief in Feni,Bangladesh
             </h2>
             <p
               class="text-base font-light secondarytxt px-4 mb-4"
