5d5ff596283ee93be58573e00b516b9a102c7fb2
mrashed21 <muhammadrashed760@gmail.com>
2024-09-24 17:21:17 +0600

    added function for feni

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -90,3 +90,72 @@ donateNoakhali.addEventListener("click", () => {
     history.appendChild(div);
   }
 });
+
+/*****Donate for Feni*****/
+const donateFeni = donateSumbit("donateFeni");
+donateFeni.addEventListener("click", () => {
+  let amountFeni = getdonateAmount("amountFeni");
+  let initialBalanceFeni = donatInitial("initialBalanceFeni");
+  if (
+    amountFeni === "" ||
+    isNaN(amountFeni) ||
+    amountFeni <= 0 ||
+    amountFeni > initialBalance
+  ) {
+    const modal = document.getElementById("donateFailed");
+    const closePopUp = document.getElementById("close-popup");
+    modal.close();
+    modal.classList.add("hidden");
+    modal.classList.remove("hidden");
+    modal.showModal();
+    closePopUp.addEventListener("click", function () {
+      document.getElementById("donateFailed").classList.add("hidden");
+    });
+  } else {
+    /* Update Donate Amount */
+    let result = parseFloat(initialBalanceFeni) + amountFeni;
+    document.getElementById("initialBalanceFeni").innerText = result;
+
+    /*update Current Balance*/
+    initialBalance -= amountFeni;
+    document.getElementById("initialBalance").innerText = initialBalance;
+
+    /*Donation Success Modal*/
+    const modal = document.getElementById("donateSuccess");
+    const closePopUp = document.getElementById("close-popup");
+    modal.close();
+    modal.classList.add("hidden");
+    modal.classList.remove("hidden");
+    modal.showModal();
+    closePopUp.addEventListener("click", function (e) {
+      document.getElementById("donateFailed").classList.add("hidden");
+    });
+
+    /*Donation History*/
+    const timeDate = new Date();
+    const time = timeDate.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    const date = timeDate.toLocaleDateString("en-US");
+    const history = document.getElementById("historyData");
+    const div = document.createElement("div");
+    div.classList.add(
+      "bg-white",
+      "shadow-md",
+      "rounded-lg",
+      "border-2",
+      "border-gray-300"
+    );
+    div.innerHTML = `<h2 class="font-medium text-xl px-4 mt-4 primary-txt">
+              ${amountFeni} Donate for Flood Relief in Feni,Bangladesh
+            </h2>
+            <p
+              class="text-base font-light secondarytxt px-4 mb-4"
+            >
+              Time: ${time} Date: ${date}
+            </p> `;
+    history.appendChild(div);
+  }
+});
