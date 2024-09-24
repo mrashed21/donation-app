ad5bdf74cb1aeae0664eb0fe7dbb8f03d5cf10e9
mrashed21 <muhammadrashed760@gmail.com>
2024-09-24 21:22:21 +0600

    added blog.html and create a common function

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -1,4 +1,4 @@
-/*****initialBalance*****/
+/*initialBalance*/
 let initial = document.getElementById("initialBalance").innerText;
 let initialBalance = parseFloat(initial);
 
@@ -22,19 +22,19 @@ historyButton.addEventListener("click", () => {
   userUI.classList.add("hidden");
 });
 
-/*****Donate for Noakhali*****/
-const donateNoakhali = donateSumbit("donateNoakhali");
-donateNoakhali.addEventListener("click", () => {
-  let amountNoakhali = getdonateAmount("amountNoakhali");
-  let initialBalanceNoakhali = donatInitial("initialBalanceNoakhali");
+/* Donation Process Common Funtion */
+function donationProcess(donetAmount, donatBalance, donetLocation, modalID) {
+  let amount = getdonateAmount(donetAmount);
+  let donateBalance = donatInitial(donatBalance);
 
   if (
-    amountNoakhali === "" ||
-    isNaN(amountNoakhali) ||
-    amountNoakhali <= 0 ||
-    amountNoakhali > initialBalance
+    amount === "" ||
+    isNaN(amount) ||
+    parseFloat(amount) <= 0 ||
+    parseFloat(amount) > initialBalance
   ) {
-    const modal = document.getElementById("donateFailed");
+    /* Modal PopUp  */
+    const modal = document.getElementById(modalID);
     const closePopUp = document.getElementById("close-popup");
     modal.close();
     modal.classList.add("hidden");
@@ -45,11 +45,11 @@ donateNoakhali.addEventListener("click", () => {
     });
   } else {
     /* Update Donate Amount */
-    let result = parseFloat(initialBalanceNoakhali) + amountNoakhali;
-    document.getElementById("initialBalanceNoakhali").innerText = result;
+    let result = parseFloat(donateBalance) + parseFloat(amount);
+    document.getElementById(donatBalance).innerText = result;
 
     /*update Current Balance*/
-    initialBalance -= amountNoakhali;
+    initialBalance -= amount;
     document.getElementById("initialBalance").innerText = initialBalance;
 
     /*Donation Success Modal*/
@@ -59,7 +59,7 @@ donateNoakhali.addEventListener("click", () => {
     modal.classList.add("hidden");
     modal.classList.remove("hidden");
     modal.showModal();
-    closePopUp.addEventListener("click", function () {
+    closePopUp.addEventListener("click", function (e) {
       document.getElementById("donateFailed").classList.add("hidden");
     });
 
@@ -81,7 +81,7 @@ donateNoakhali.addEventListener("click", () => {
       "border-gray-300"
     );
     div.innerHTML = `<h2 class="font-medium text-xl px-4 mt-4 primary-txt">
-              ${amountNoakhali} Donate for Flood at Noakhali, Bangladesh
+              ${amount} Taka is Donated for ${donetLocation}, Bangladesh
             </h2>
             <p
               class="text-base font-light secondarytxt px-4 mb-4"
@@ -90,142 +90,37 @@ donateNoakhali.addEventListener("click", () => {
             </p> `;
     history.appendChild(div);
   }
+}
+
+/*****Donate for Noakhali*****/
+const donateNoakhali = donateSumbit("donateNoakhali");
+donateNoakhali.addEventListener("click", () => {
+  donationProcess(
+    "amountNoakhali",
+    "initialBalanceNoakhali",
+    "famine-2024 at Noakhali",
+    "donateFailed"
+  );
 });
 
 /*****Donate for Feni*****/
 const donateFeni = donateSumbit("donateFeni");
 donateFeni.addEventListener("click", () => {
-  let amountFeni = getdonateAmount("amountFeni");
-  let initialBalanceFeni = donatInitial("initialBalanceFeni");
-  if (
-    amountFeni === "" ||
-    isNaN(amountFeni) ||
-    amountFeni <= 0 ||
-    amountFeni > initialBalance
-  ) {
-    const modal = document.getElementById("donateFailed");
-    const closePopUp = document.getElementById("close-popup");
-    modal.close();
-    modal.classList.add("hidden");
-    modal.classList.remove("hidden");
-    modal.showModal();
-    closePopUp.addEventListener("click", function () {
-      document.getElementById("donateFailed").classList.add("hidden");
-    });
-  } else {
-    /* Update Donate Amount */
-    let result = parseFloat(initialBalanceFeni) + amountFeni;
-    document.getElementById("initialBalanceFeni").innerText = result;
-
-    /*update Current Balance*/
-    initialBalance -= amountFeni;
-    document.getElementById("initialBalance").innerText = initialBalance;
-
-    /*Donation Success Modal*/
-    const modal = document.getElementById("donateSuccess");
-    const closePopUp = document.getElementById("close-popup");
-    modal.close();
-    modal.classList.add("hidden");
-    modal.classList.remove("hidden");
-    modal.showModal();
-    closePopUp.addEventListener("click", function (e) {
-      document.getElementById("donateFailed").classList.add("hidden");
-    });
-
-    /*Donation History*/
-    const timeDate = new Date();
-    const time = timeDate.toLocaleTimeString("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    });
-    const date = timeDate.toLocaleDateString("en-US");
-    const history = document.getElementById("historyData");
-    const div = document.createElement("div");
-    div.classList.add(
-      "bg-white",
-      "shadow-md",
-      "rounded-lg",
-      "border-2",
-      "border-gray-300"
-    );
-    div.innerHTML = `<h2 class="font-medium text-xl px-4 mt-4 primary-txt">
-              ${amountFeni} Donate for Flood Relief in Feni,Bangladesh
-            </h2>
-            <p
-              class="text-base font-light secondarytxt px-4 mb-4"
-            >
-              Time: ${time} Date: ${date}
-            </p> `;
-    history.appendChild(div);
-  }
+  donationProcess(
+    "amountFeni",
+    "initialBalanceFeni",
+    "Flood Relief in Feni",
+    "donateFailed"
+  );
 });
 
 /*****Donate for Quota*****/
 const donateQuota = donateSumbit("donateQuota");
 donateQuota.addEventListener("click", () => {
-  const amountQuota = getdonateAmount("amountQuota");
-  let initialBalanceQuota = donatInitial("initialBalanceQuota");
-  if (
-    amountQuota === "" ||
-    isNaN(amountQuota) ||
-    amountQuota <= 0 ||
-    amountQuota > initialBalance
-  ) {
-    const modal = document.getElementById("donateFailed");
-    const closePopUp = document.getElementById("close-popup");
-    modal.close();
-    modal.classList.add("hidden");
-    modal.classList.remove("hidden");
-    modal.showModal();
-    closePopUp.addEventListener("click", function () {
-      document.getElementById("donateFailed").classList.add("hidden");
-    });
-  } else {
-    /* Update Donate Amount */
-    let result = parseFloat(initialBalanceQuota) + amountQuota;
-    document.getElementById("initialBalanceQuota").innerText = result;
-
-    /*update Current Balance*/
-    initialBalance -= amountQuota;
-    document.getElementById("initialBalance").innerText = initialBalance;
-
-    /*Donation Success Modal*/
-    const modal = document.getElementById("donateSuccess");
-    const closePopUp = document.getElementById("close-popup");
-    modal.close();
-    modal.classList.add("hidden");
-    modal.classList.remove("hidden");
-    modal.showModal();
-    closePopUp.addEventListener("click", function (e) {
-      document.getElementById("donateFailed").classList.add("hidden");
-    });
-
-    /*Donation History*/
-    const timeDate = new Date();
-    const time = timeDate.toLocaleTimeString("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    });
-    const date = timeDate.toLocaleDateString("en-US");
-    const history = document.getElementById("historyData");
-    const div = document.createElement("div");
-    div.classList.add(
-      "bg-white",
-      "shadow-md",
-      "rounded-lg",
-      "border-2",
-      "border-gray-300"
-    );
-    div.innerHTML = `<h2 class="font-medium text-xl px-4 mt-4 primary-txt">
-              ${amountQuota} Donate for Flood Relief in Feni,Bangladesh
-            </h2>
-            <p
-              class="text-base font-light secondarytxt px-4 mb-4"
-            >
-              Time: ${time} Date: ${date}
-            </p> `;
-    history.appendChild(div);
-  }
+  donationProcess(
+    "amountQuota",
+    "initialBalanceQuota",
+    "Aid for Injured in the Quota Movement",
+    "donateFailed"
+  );
 });
--- assets/JavaScript/utilities.js
+++ assets/JavaScript/utilities.js
@@ -1,13 +1,16 @@
+/* Input Amount Common Funtion */
 function getdonateAmount(id) {
   let amount = document.getElementById(id).value;
-  return parseFloat(amount);
+  return amount;
 }
 
+/* Donation Button Common Funtion */
 function donateSumbit(id) {
   const donateButton = document.getElementById(id);
   return donateButton;
 }
 
+/* Total Donation Amount Common Funtion */
 function donatInitial(id) {
   const donatInitialBalance = document.getElementById(id).innerText;
   return donatInitialBalance;
